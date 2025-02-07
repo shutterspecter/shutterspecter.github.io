55e2313c1ac07dd11f4577ad88787722012c803d
shutterspecter <124100244+shutterspecter@users.noreply.github.com>
2025-02-07 20:14:48 +0200

    Restructuring main page

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 // Smooth scroll for nav links
 document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      const targetSection = document.querySelector(this.getAttribute('href'));
-      targetSection.scrollIntoView({ behavior: 'smooth' });
-      
-      // Close mobile nav (if open)
-      if (window.innerWidth <= 768) {
-        document.querySelector('.nav-links').style.display = 'none';
-      }
-    });
-  });
-  
-  // Menu toggle for mobile
-  const menuToggle = document.querySelector('.menu-toggle');
-  const navLinks = document.querySelector('.nav-links');
-  
-  menuToggle.addEventListener('click', () => {
-    // If navLinks is hidden, display it, else hide it
-    if (navLinks.style.display === 'flex') {
-      navLinks.style.display = 'none';
-    } else {
-      navLinks.style.display = 'flex';
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetSection = document.querySelector(this.getAttribute('href'));
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+    
+    // Close mobile nav (if open)
+    if (window.innerWidth <= 768) {
+      document.querySelector('.nav-links').style.display = 'none';
     }
   });
+});
+
+// Menu toggle for mobile
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  // If navLinks is hidden, display it, else hide it
+  if (navLinks.style.display === 'flex') {
+    navLinks.style.display = 'none';
+  } else {
+    navLinks.style.display = 'flex';
+  }
+});
