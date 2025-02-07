8aba4504eac29ed0875504e384fb374a9ea7f122
shutterspecter <124100244+shutterspecter@users.noreply.github.com>
2025-02-07 13:39:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetSection = document.querySelector(this.getAttribute('href'));
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
